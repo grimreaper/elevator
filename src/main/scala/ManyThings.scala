9491abed620b1e0e3d40e3dffc95f00389b79dab
Eitan Adler <lists@eitanadler.com>
2017-10-13 00:09:42 -0700

    Add more junk to play with the compiler

--- src/main/scala/ManyThings.scala
+++ src/main/scala/ManyThings.scala
@@ -1,11 +1,36 @@
+
 //noinspection ScalaFileName
-trait Provider {
+trait GenericMatchedID {
+}
+
+
+trait Providing extends GenericMatchedID {
+  trait Providable {
+  }
+
+  trait Provider {
+
+  }
 
 }
 
-trait Builder {
-}
 
-trait PartialBuilder extends Builder {
-}
 
+
+trait Building extends GenericMatchedID{
+  trait Builder {
+  }
+
+  trait Buidlable {
+  }
+
+  trait PartialBuilding extends GenericMatchedID{
+
+    trait PartiallyBuildable {
+    }
+
+    trait PartialBuilder extends Builder {
+    }
+
+  }
+}
